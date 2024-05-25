4e8c4625602bce3ed53255f8b60a8724dd7ab1bf
monu-here <tmonu995@gmail.com>
2024-05-25 22:12:07 +0545

    user update

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -25,5 +25,6 @@ class DatabaseSeeder extends Seeder
         $this->call(StudentAttendence::class);
         $this->call(RoleSeeder::class);
         $this->call(FaculitySeeder::class);
+        $this->call(UserSeeder::class);
     }
 }
--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -14,7 +14,7 @@ class UserSeeder extends Seeder
     public function run(): void
     {
         User::create([
-            'name' => 'Super Admin',
+            'name' => 'SuperAdmin',
         ]);
     }
 }
